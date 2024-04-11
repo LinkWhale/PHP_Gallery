f772d78de490d75ef3f79b48503212de32852fab
LinkWhale <filipbjed@gmail.com>
2024-04-11 09:59:57 +0200

    file-upload-fixes

--- upload.php
+++ upload.php
@@ -22,41 +22,38 @@
     
 <?php
 if(isset($_POST["submit"])) {
-
+    try {
         //Image upload
-        $target_dir = "img/";
-        $target_file = $target_dir . $_FILES["file"]["name"];
-        $uploadOk = 1;
-        if(file_exists($target_file)) {
-            $uploadOk = 0;
-        }
-    
-        // Check if its an image
-        try {
-            $check = getimagesize($_FILES["file"]["tmp_name"]);
-        
-            if($check !== false) {
-                $uploadOk = 1;
-            } else {
+            $target_dir = "img/";
+            $target_file = $target_dir . $_FILES["file"]["name"];
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            if(file_exists($target_file)) {
                 $uploadOk = 0;
-                echo "error file is not an image";
-                }
+                echo "File already exists. ";
+            }
 
             // Check if file is small enough
             if($_FILES["file"]["size"] > 5000000) {
                 $uploadOk = 0;
-                echo "Error file too big";  
+                echo "Error file too big. ";  
             }
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed. ";
+                $uploadOk = 0;
+                }
 
             if($uploadOk == 1) {
                 move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
                 echo $target_file;
                 echo "<img src=".$target_file." width=300>";
             }
-        }
-        catch(ValueError $e) {
-            echo "No file uploaded";
-        }
+
+            
+    }
+    catch(ValueError $e) {
+        echo "No file uploaded";
+    }
     }?>
     </form>
 </body>
